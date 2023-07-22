b914a9be61cd70ee0da73ae3a3e729590e83bcf3
Stephen <pharion3d@gmail.com>
2023-07-22 18:49:51 +0100

    Initial commit

---
+++ GoodbyeLife/GoodbyeLife/Program.cs
@@ -0,0 +1,46 @@
+﻿using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GoodbyeLife
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Lets play noughts & crosses");
+            //Console.ReadLine();
+
+            // Create a driver instance for chromedriver
+            IWebDriver driver = new ChromeDriver();
+
+            //Navigate to google page
+            driver.Navigate().GoToUrl("http:www.google.com");
+
+            //Maximize the window
+            driver.Manage().Window.Maximize();
+
+           
+            //Find the Search text box using xpath
+            IWebElement  beforeYouContinue = driver.FindElement(By.Id("W0wltc"));
+            beforeYouContinue.Click();
+
+            //Find the Search text box using xpath
+            IWebElement element = driver.FindElement(By.XPath("//*[@title='Search']"));
+
+            //Enter some text in search text box
+            element.SendKeys("tic-tac-toe");
+
+            IWebElement middleX = driver.FindElement(By.XPath("xpath=//tr[2]/td[2]"));
+
+            middleX.Click();
+
+            //Close the browser
+            driver.Close();
+        }
+    }
+}
